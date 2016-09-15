9bceecf2a6ecbbd928d1905bd9293a123bcb7911
Jaromir Hamala <jaromir.hamala@gmail.com>
2016-09-15 18:45:00 +0300

    additional ad-hoc metrics

--- pMetrics.py
+++ pMetrics.py
@@ -9,13 +9,21 @@ pending_invocations_template = """
             <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
         </head>
     <body>
-        <h1>Metrics</h1>
+        <a id="toc">
+            <h1>Metrics</h1>
+        <a/>
+        <ul>
+            {{#data}}
+                <li><a href="#{{name}}">{{name}}</a>
+            {{/data}}
+        </ul>
         {{#data}}
-            <h2>{{name}}</h2>
+            <a id="{{name}}"><h2>{{name}}</h2></a>
             {{{plot}}}
+            <a href="#toc">[Table of Content]</a>
         {{/data}}
     </body>
-</html>
+</html>t
 """
 
 # SimpleMetricPlotter('operation.invocations.pending').plot(pending_invocations_template)
@@ -30,6 +38,8 @@ SimpleMetricPlotter([   'operation.completedCount',
                         'operation.invocations.responses[error]',
                         'operation.invocations.responses[missing]',
                         'operation.invocations.responses[timeout]',
+                        'operation.invocations.heartbeatPacketsReceived',
+                        'operation.invocations.backupTimeouts',
                         'cluster.clock.clusterTimeDiff',
                         'event.eventQueueSize',
                         'event.totalFailureCount',
@@ -38,4 +48,37 @@ SimpleMetricPlotter([   'operation.completedCount',
                         'executor.[hz:scheduled].completedTaskCount',
                         'partitions.replicaSyncRequestsCounter',
                         'gc.majorTime',
+
+                        'tcp.inputThread[hz._hzInstance_1_workers.IO.thread-in-0].eventCount',
+                        'tcp.inputThread[hz._hzInstance_1_workers.IO.thread-in-1].eventCount',
+                        'tcp.inputThread[hz._hzInstance_1_workers.IO.thread-in-2].eventCount',
+                        'tcp.outputThread[hz._hzInstance_1_workers.IO.thread-out-0].eventCount',
+                        'tcp.outputThread[hz._hzInstance_1_workers.IO.thread-out-1].eventCount',
+                        'tcp.outputThread[hz._hzInstance_1_workers.IO.thread-out-2].eventCount',
+
+                        'operation.thread[hz._hzInstance_1_workers.generic-operation.thread-0].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.generic-operation.thread-1].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.generic-operation.thread-2].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.generic-operation.thread-3].completedTotalCount',
+
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-0].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-1].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-2].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-3].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-4].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-5].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-6].completedTotalCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-7].completedTotalCount',
+
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-0].normalPendingCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-1].normalPendingCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-2].normalPendingCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-3].normalPendingCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-4].normalPendingCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-5].normalPendingCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-6].normalPendingCount',
+                        'operation.thread[hz._hzInstance_1_workers.partition-operation.thread-7].normalPendingCount'
+
+
+
                      ]).plot(pending_invocations_template)
